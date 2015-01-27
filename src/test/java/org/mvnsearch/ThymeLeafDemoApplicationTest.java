ca76d528a4e68a1640ef65daa4b129623d221504
linux_china <linux_china@hotmail.com>
2015-01-27 10:09:42 +0800

    thymeleaf applicaiton test

---
+++ src/test/java/org/mvnsearch/ThymeLeafDemoApplicationTest.java
@@ -0,0 +1,22 @@
+package org.mvnsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * thymeleaf applicaiton test
+ *
+ * @author linux_china
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ThymeLeafDemoApplication.class)
+public class ThymeLeafDemoApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
