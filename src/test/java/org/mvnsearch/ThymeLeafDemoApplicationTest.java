eb71918c67650b1be3c396bcccf43e2ef3ad16cd
linux_china <linux_china@hotmail.com>
2016-05-25 02:23:27 +0800

    minor update

--- src/test/java/org/mvnsearch/ThymeLeafDemoApplicationTest.java
+++ src/test/java/org/mvnsearch/ThymeLeafDemoApplicationTest.java
@@ -2,21 +2,28 @@ package org.mvnsearch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * thymeleaf applicaiton test
  *
  * @author linux_china
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ThymeLeafDemoApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class ThymeLeafDemoApplicationTest {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @Test
     public void contextLoads() {
-
+        redisTemplate.boundValueOps("nick").set("Jacky");
+        System.out.println("nick:" + redisTemplate.boundValueOps("nick").get());
     }
 
 }
