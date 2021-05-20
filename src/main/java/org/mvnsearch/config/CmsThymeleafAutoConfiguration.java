53ba0f34ad246f94f2a4b371170527bb55c36858
linux_china <libing.chen@gmail.com>
2021-05-20 13:24:42 -0700

    Polishing

--- src/main/java/org/mvnsearch/config/CmsThymeleafAutoConfiguration.java
+++ src/main/java/org/mvnsearch/config/CmsThymeleafAutoConfiguration.java
@@ -3,11 +3,9 @@ package org.mvnsearch.config;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring4.SpringTemplateEngine;
 
 /**
  * cms thymeleaf auto configuration
@@ -15,7 +13,6 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
  * @author linux_china
  */
 @Configuration
-@ConditionalOnClass(SpringTemplateEngine.class)
 @AutoConfigureBefore(ThymeleafAutoConfiguration.class)
 @AutoConfigureAfter({CacheAutoConfiguration.class})
 public class CmsThymeleafAutoConfiguration {
