4a0078040b154f05a3b33e31cc6520c8df0f8ab6
Libing Chen <libing.chen@gmail.com>
2022-12-02 08:52:14 +0800

    feat: add htmxRequest

--- src/main/java/org/mvnsearch/controllers/HomeController.java
+++ src/main/java/org/mvnsearch/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package org.mvnsearch.controllers;
 
+import io.github.wimdeblauwe.hsbt.mvc.HtmxRequest;
+import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -11,5 +15,12 @@ public class HomeController {
     String index(Model model) {
         model.addAttribute("nick", "Jack");
         return "index";
+    }
+
+    @GetMapping("/partial")
+    @HxRequest
+    @ResponseBody
+    public String htmxRequest(HtmxRequest details) {
+        return "good morning";
     }
 }
